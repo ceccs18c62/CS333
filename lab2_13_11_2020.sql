fffb1bc080f29eecf02957ff5b89243410cd8d6f
ceccs18c62 <60084945+ceccs18c62@users.noreply.github.com>
2020-11-13 12:11:10 +0530

    Update lab2_13_11_2020.sql

--- lab2_13_11_2020.sql
+++ lab2_13_11_2020.sql
@@ -6,18 +6,22 @@ CREATE TABLE Employee (
   dop date,
   salary int
 );
+
 -- inserting two tuples
 INSERT into Employee
 VALUES("e1","Abi","leader","1995-02-12",45000);
 INSERT into Employee
 VALUES("e2","sam","manager","1993-03-24",58000);
+
 -- selecting all details
 SELECT * FROM Employee;
+
 -- changing salary  
 UPDATE Employee
 SET salary=48000
 WHERE code="e1";
 SELECT * FROM Employee;
+
 -- deleting a tuple
 DELETE FROM Employee
 WHERE code="e2";
