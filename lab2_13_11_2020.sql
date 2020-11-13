7596a850c84ddc6ecbcf6e8654a60bef7c13db94
ceccs18c62 <60084945+ceccs18c62@users.noreply.github.com>
2020-11-13 12:09:33 +0530

    Add files via upload

---
+++ lab2_13_11_2020.sql
@@ -0,0 +1,24 @@
+-- create a table
+CREATE TABLE Employee (
+  code varchar(4),
+  name varchar(10),
+  designation varchar(30),
+  dop date,
+  salary int
+);
+-- inserting two tuples
+INSERT into Employee
+VALUES("e1","Abi","leader","1995-02-12",45000);
+INSERT into Employee
+VALUES("e2","sam","manager","1993-03-24",58000);
+-- selecting all details
+SELECT * FROM Employee;
+-- changing salary  
+UPDATE Employee
+SET salary=48000
+WHERE code="e1";
+SELECT * FROM Employee;
+-- deleting a tuple
+DELETE FROM Employee
+WHERE code="e2";
+SELECT * FROM Employee;
